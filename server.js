1f6fc25422711523795ac9c8b52a098365eba2d3
Charity2010 <zfasa3214@westada.org>
2026-03-13 14:17:38 +0000

    Added pages and navigation

--- server.js
+++ server.js
@@ -14,14 +14,25 @@ app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`)
 })
 
-app.get('/about', (req, res) => {
-    res.send("<h1>About Me</h1>")
-})
+// app.get('/about', (req, res) => {
+//     res.send("<h1>About Me</h1>")
+// })
 
-app.get('/', (req, res) => {
-    res.send("<h1>Welcome to My Website</h1>")
-})
+// app.get('/', (req, res) => {
+//     res.send("<h1>Welcome to My Website</h1>")
+// })
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`)
 })
 
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html')
+})
+
+app.get('/about', (req, res) => {
+  res.sendFile(__dirname + '/public/about.html')
+})
+
+app.get('/contact', (req, res) => {
+  res.sendFile(__dirname + '/public/contact.html')
+})
